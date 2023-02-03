543fcc6f52f94514bb74882d6cd2cde4f2b0b9b7
HwangGoeun <82012751+HwangGoeun@users.noreply.github.com>
2023-02-03 15:44:02 +0900

    라이다 인식 범위 수정

--- lidar/src/test.py
+++ lidar/src/test.py
@@ -11,14 +11,15 @@ if __name__ == '__main__':
     for scan in env.scanning():
         count += 1
 
-        left = env.getAngleDistanceRange(scan, 0, 30, 0, 400)
-        go = env.getAngleDistanceRange(scan, 31, 120, 0, 300)
-        right = env.getAngleDistanceRange(scan, 121, 180, 400, 800)
+        left = env.getAngleDistanceRange(scan, 330, 360, 800, 1000)
+        go = env.getAngleDistanceRange(scan, 0, 40, 0, 400)
+        right = env.getAngleDistanceRange(scan, 41, 60, 400, 600)
+        go2 = env.getAngleDistanceRange(scan, 61, 120, 0, 400)
 
         if len(left):
             print("left")
         else:
-            if len(go):
+            if len(go) or len(go2):
                 print("go")
             else:
                 if len(right):
